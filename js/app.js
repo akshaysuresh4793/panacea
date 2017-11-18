657d76bbddc1fa0d6960c2efe49da44530a51d79
Akshay Suresh <akshay.suresh.4793@gmail.com>
2017-11-18 03:27:41 +0000

    add datafactory

--- js/app.js
+++ js/app.js
@@ -18,3 +18,75 @@ app.config(function($routeProvider) {
 		controller : "ProductController" 
 	})
 })
+
+app.factory('dataFactory',['$http', function($http) {
+	var hostName = 'localhost';
+	var port = '3000';
+	var protocol = 'http';
+	var baseUrl = protocol + "://" + hostName + ":" + port;
+
+	var dataFactory = {};
+
+	// Products
+	dataFactory.getProducts = function() {
+		return $http.get(baseUrl + "/" + "Products");
+	}
+
+	dataFactory.getProduct = function(id) {
+		return $http.get(baseUrl + "/" + "Products/" + id);
+	}
+
+	dataFactory.insertProduct = function(obj) {
+		return $http.post(baseUrl + "/" + "Products", obj);
+	}
+
+	dataFactory.updateProduct = function(id, obj) {
+		return $http.put(baseUrl + "/" + "Products/" + id, obj);
+	}
+
+	dataFactory.deleteProduct = function(id) {
+		return $http.delete(baseUrl + "/" + "Products/" + id);
+	}
+
+	 // People
+ 	dataFactory.getPeople = function() {
+        	return $http.get(baseUrl + "/" + "People"); 
+	}
+
+	dataFactory.getPerson = function(id) {
+        	return $http.get(baseUrl + "/" + "People/" + id);
+	}
+
+	dataFactory.insertPerson = function(obj) {
+        	return $http.post(baseUrl + "/" + "People", obj);
+ 	}
+
+	dataFactory.updatePerson = function(id, obj) {
+        	return $http.put(baseUrl + "/" + "People/" + id, obj);
+	}
+
+	dataFactory.deletePerson = function(id) {
+        	return $http.delete(baseUrl + "/" + "People/" + id);
+	}
+
+	// Houses
+	dataFactory.getHouses = function() {
+        	return $http.get(baseUrl + "/" + "Houses");
+	}
+
+	dataFactory.getHouse = function(id) {
+        	return $http.get(baseUrl + "/" + "Houses/" + id);
+	}
+
+	dataFactory.insertHouse = function(obj) {
+        	return $http.post(baseUrl + "/" + "Houses", obj);
+	}
+
+	dataFactory.updateHouse = function(id, obj) {
+        	return $http.put(baseUrl + "/" + "Houses/" + id, obj);
+	}
+
+	dataFactory.deleteHouse = function(id) {
+        	return $http.delete(baseUrl + "/" + "Houses/" + id);
+	}	
+}]);
